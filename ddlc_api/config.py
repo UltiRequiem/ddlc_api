3600bbf332da29d40cacbcce4cd0855bfcb697bf
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-11-05 15:38:46 -0500

    :recycle: Update

--- ddlc_api/config.py
+++ ddlc_api/config.py
@@ -1,4 +1,4 @@
 import os
 
-DEV = os.environ.get("FLASK_ENV") == "true"
+DEV = os.environ.get("FLASK_ENV") == "development"
 URL = "http://localhost:5000" if DEV else "https://ddlcapi.herokuapp.com"
